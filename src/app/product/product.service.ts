1c1b7a0abb2d0e9ce8abbf3e64e35f8d7fe1e166
akshatkumar <akshatk806@gmail.com>
2025-02-10 21:32:19 +0530

    get request to get all products from mock api

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private apiUrl = environment.apiUrl + "/products";        // we would send a get request to localhost:3000/products on development
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  // get request to get all products
+  getProducts(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(this.apiUrl);
+  }
 }
