8c522778fb10a642216667dae8b1d6e5e81c02ca
akshatkumar <akshatk806@gmail.com>
2025-02-13 21:27:48 +0530

    added cart service to get, post and delete cart items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { CartModule } from './cart/cart.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { MatButtonModule } from '@angular/material/button';
     AppRoutingModule,
     ProductModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    CartModule
   ],
   providers: [
     provideClientHydration(withEventReplay()),
--- src/app/cart/cart.service.ts
+++ src/app/cart/cart.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../models/product';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  private apiUrl = environment.apiUrl + "/cart";
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  addToCart(product: Product): Observable<Product> {
+    return this.httpClient.post<Product>(this.apiUrl, product);
+  }
+
+  getCartItems(): Observable<Product[]>{
+    return this.httpClient.get<Product[]>(this.apiUrl);
+  }
+
+  clearCart(): Observable<void> {
+    return this.httpClient.delete<void>(this.apiUrl);
+  } 
 }
